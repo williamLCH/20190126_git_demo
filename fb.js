f27f620430b2ba4f6b436bafeadfa719d18ebadd
williamLCH <47047464+williamLCH@users.noreply.github.com>
2019-02-16 14:07:57 +0800

    change remote fb.js

---
+++ fb.js
@@ -0,0 +1,7 @@
+Reuters) - Suspects arrested in connection with an alleged racist and homophobic assault on "Empire" actor Jussie Smollett in Chicago were released on Friday, police said.
+
+"Due to new evidence as a result of today's interrogations, the individuals questioned by police in the Empire case have now been released without charging and detectives have additional investigative work to complete," the Chicago Police Department said in a email to Reuters.
+
+The individuals are no longer being classified as suspects, the department said.
+
+A spokesman for the department on Friday night declined to confirm to Reuters whether the individuals released were the two Nigerian brothers police said they arrested on Wednesday at 
