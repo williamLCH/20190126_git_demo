35b31a4fcec93459174e567caadee88e3c158249
williamLCH <B10002235@gmail.com>
2019-02-16 16:05:51 +0800

    fb.js add

--- fb.js
+++ fb.js
@@ -1,9 +1,12 @@
 
+
+
 彭博資訊在統整主要避險基金的上季持股變化後發現，蘋果與微軟股票市遭減持最多的股票，至少三家避險基金都跟隨巴菲特的腳步，減碼蘋果持股，也有九家避險基金減碼微軟股票、還有兩家出清持股。
 
 更多udn報導
 當盤仔！女子捐400萬求神明保平安 竟只換到4個字
 小資女炫耀存款達「8位數」 網友糾錯空歡喜一場
+
 3.5萬名牌鞋穿三天就壞 櫃姐竟回：中國沒有技術修復
 
 Reuters) - Suspects arrested in connection with an alleged racist and homophobic assault on "Empire" actor Jussie Smollett in Chicago were released on Friday, police said.
@@ -12,4 +15,6 @@ Reuters) - Suspects arrested in connection with an alleged racist and homophobic
 
 The individuals are no longer being classified as suspects, the department said.
 
-A spokesman for the department on Friday night declined to confirm to Reuters whether the individuals released were the two Nigerian brothers police said they arrested on Wednesday at 
+
+3.5萬名牌鞋穿三天就壞 櫃姐竟回：中國沒有技術修復
+
