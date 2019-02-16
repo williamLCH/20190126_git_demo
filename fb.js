b8268ed0d86ef506a30389146f55d42b80cdf2de
williamLCH <B10002235@gmail.com>
2019-02-16 16:06:20 +0800

    fb.js

--- fb.js
+++ fb.js
@@ -1,6 +1,7 @@
 
 
 
+
 彭博資訊在統整主要避險基金的上季持股變化後發現，蘋果與微軟股票市遭減持最多的股票，至少三家避險基金都跟隨巴菲特的腳步，減碼蘋果持股，也有九家避險基金減碼微軟股票、還有兩家出清持股。
 
 更多udn報導
@@ -17,4 +18,13 @@ The individuals are no longer being classified as suspects, the department said.
 
 
 3.5萬名牌鞋穿三天就壞 櫃姐竟回：中國沒有技術修復
+
+
+Reuters) - Suspects arrested in connection with an alleged racist and homophobic assault on "Empire" actor Jussie Smollett in Chicago were released on Friday, police said.
+
+"Due to new evidence as a result of today's interrogations, the individuals questioned by police in the Empire case have now been released without charging and detectives have additional investigative work to complete," the Chicago Police Department said in a email to Reuters.
+
+The individuals are no longer being classified as suspects, the department said.
+
+A spokesman for the department on Friday night declined to confirm to Reuters whether the individuals released were the two Nigerian brothers police said they arrested on Wednesday at 
 
